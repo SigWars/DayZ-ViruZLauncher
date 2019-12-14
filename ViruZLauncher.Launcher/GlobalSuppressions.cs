43c320072437ec84bcf90ade0d3ef0f8da983ded
SigWar <highnetlan@gmail.com>
2019-12-14 15:55:25 -0300

    Fixed Change name Added more functions edit Ini Configuration changed to new server

---
+++ ViruZLauncher.Launcher/GlobalSuppressions.cs
@@ -0,0 +1,33 @@
+﻿//
+//  GlobalSuppressions.cs
+//
+//  Author:
+//       Jarl Gullberg <jarl.gullberg@gmail.com>
+//
+//  Copyright (c) 2017 Jarl Gullberg
+//
+//  This program is free software: you can redistribute it and/or modify
+//  it under the terms of the GNU General Public License as published by
+//  the Free Software Foundation, either version 3 of the License, or
+//  (at your option) any later version.
+//
+//  This program is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//  GNU General Public License for more details.
+//
+//  You should have received a copy of the GNU General Public License
+//  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+//
+
+[assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("Estilo", "IDE0044:Adicionar modificador somente leitura", Justification = "<Pendente>", Scope = "member", Target = "~F:ViruZLauncher.Launcher.IniFile.Path")]
+[assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("StyleCop.CSharp.DocumentationRules", "SA1614:Element parameter documentation should have text", Justification = "<Pendente>", Scope = "member", Target = "~M:ViruZLauncher.Launcher.IniFile.#ctor(System.String)")]
+[assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("StyleCop.CSharp.NamingRules", "SA1305:Field names should not use Hungarian notation", Justification = "<Pendente>", Scope = "member", Target = "~M:ViruZLauncher.Launcher.Interface.MainWindow.OnMenuChangeNameActivated(System.Object,System.EventArgs)")]
+[assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("StyleCop.CSharp.DocumentationRules", "SA1636:File header copyright text should match", Justification = "<Pendente>")]
+[assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("StyleCop.CSharp.DocumentationRules", "SA1617:Void return value should not be documented", Justification = "<Pendente>", Scope = "member", Target = "~M:ViruZLauncher.Launcher.Interface.ChangeNameWindow.BindUIEvents")]
+[assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("StyleCop.CSharp.ReadabilityRules", "SA1130:Use lambda syntax", Justification = "<Pendente>", Scope = "member", Target = "~M:ViruZLauncher.Launcher.InputBox.Show(System.String,System.String,System.String@,ViruZLauncher.Launcher.InputBoxValidation)~System.Windows.Forms.DialogResult")]
+[assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("StyleCop.CSharp.MaintainabilityRules", "SA1119:Statement should not use unnecessary parenthesis", Justification = "<Pendente>", Scope = "member", Target = "~M:ViruZLauncher.Launcher.InputBox.Show(System.String,System.String,System.String@,ViruZLauncher.Launcher.InputBoxValidation)~System.Windows.Forms.DialogResult")]
+[assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("StyleCop.CSharp.ReadabilityRules", "SA1122:Use string.Empty for empty strings", Justification = "<Pendente>", Scope = "member", Target = "~M:ViruZLauncher.Launcher.InputBox.Show(System.String,System.String,System.String@,ViruZLauncher.Launcher.InputBoxValidation)~System.Windows.Forms.DialogResult")]
+[assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("StyleCop.CSharp.ReadabilityRules", "SA1130:Use lambda syntax", Justification = "<Pendente>", Scope = "member", Target = "~F:ViruZLauncher.Launcher.Interface.MainWindow.validation")]
+[assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("StyleCop.CSharp.ReadabilityRules", "SA1122:Use string.Empty for empty strings", Justification = "<Pendente>", Scope = "member", Target = "~F:ViruZLauncher.Launcher.Interface.MainWindow.validation")]
+[assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("StyleCop.CSharp.MaintainabilityRules", "SA1119:Statement should not use unnecessary parenthesis", Justification = "<Pendente>", Scope = "member", Target = "~F:ViruZLauncher.Launcher.Interface.MainWindow.validation")]
